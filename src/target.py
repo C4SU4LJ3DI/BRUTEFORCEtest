e242fa2a8287ab788305c51413ea9286583a6c57
C4SU4L <jds9c44sval38sd0@onionmail.com>
2025-06-12 19:20:56 +0200

    Update target.py

--- src/target.py
+++ src/target.py
@@ -1,16 +1,24 @@
 import requests
 
-def try_password(url, username, password, success_string, fail_string):
-    # Dostosuj do formatu logowania na stronie!
-    data = {
-        "username": username,
-        "password": password
-    }
+def try_password(url, username, password, success_string, fail_string,
+                 login_field, password_field, submit_field, extra_fields, method):
+    data = {}
+    if login_field:
+        data[login_field] = username
+    if password_field:
+        data[password_field] = password
+    if submit_field:
+        data[submit_field] = "Zaloguj"
+    if extra_fields:
+        data.update(extra_fields)
+
     try:
-        r = requests.post(url, data=data, timeout=5)
-        # Jeśli zalogowanie się powiodło, w odpowiedzi powinien być success_string
+        if method == "post":
+            r = requests.post(url, data=data, timeout=5, allow_redirects=True)
+        else:
+            r = requests.get(url, params=data, timeout=5, allow_redirects=True)
         if success_string in r.text and (not fail_string or fail_string not in r.text):
             return True
-    except Exception as e:
+    except Exception:
         pass
     return False
